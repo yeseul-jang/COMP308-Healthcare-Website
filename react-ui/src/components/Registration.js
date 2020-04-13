56f5a05f80bf18b73b660125003994ed37fb5911
Yeseul Jang <jangysseul@gmail.com>
2020-04-13 09:38:18 -0400

    remove unnecessary library

--- react-ui/src/components/Registration.js
+++ react-ui/src/components/Registration.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Spinner from 'react-bootstrap/Spinner';
 import { withRouter } from 'react-router-dom';
-import DatePicker from 'react-datepicker';
 
 import './Registration.css'
 
