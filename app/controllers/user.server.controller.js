016c6d767b031b6f9a6df3343b5fa83a4125d372
KyungjinJeong <57504664+KyungjinJeong@users.noreply.github.com>
2020-04-13 05:41:18 -0400

    patient search

--- app/controllers/user.server.controller.js
+++ app/controllers/user.server.controller.js
@@ -3,56 +3,56 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const config = require('../../config/config');
 const jwtExpirySeconds = 300;
-const jwtKey =config.secretKey;
+const jwtKey = config.secretKey;
 
-exports.createUser = function (req, res, next) {  
-    var user = new User(req.body); //get data from ejs page and attaches them to the model
+exports.createUser = function (req, res, next) {
+	var user = new User(req.body); //get data from ejs page and attaches them to the model
 
-	console.log(">>>>>"+req.body.usertype);
-    user.save(function (err) {
-        if (err) {
-            // Call the next middleware with an error message
-            return next(err);
-        } else {
-            // Use the 'response' object to send a JSON response
-            res.json(user);
-            
-        }
-    });
+	console.log(">>>>>" + req.body.usertype);
+	user.save(function (err) {
+		if (err) {
+			// Call the next middleware with an error message
+			return next(err);
+		} else {
+			// Use the 'response' object to send a JSON response
+			res.json(user);
+
+		}
+	});
 };
 
 
-exports.update = function(req, res, next) {
-    console.log(req.body);
-    User.findByIdAndUpdate(req.user.id, req.body, function (err, user) {
-      if (err) {
-        console.log(err);
-        return next(err);
-      }
-      res.json(user);
-    });
+exports.update = function (req, res, next) {
+	console.log(req.body);
+	User.findByIdAndUpdate(req.user.id, req.body, function (err, user) {
+		if (err) {
+			console.log(err);
+			return next(err);
+		}
+		res.json(user);
+	});
 };
 
-exports.delete = function(req, res, next) {
-    User.findByIdAndRemove(req.user.id, req.body, function (err, user) {
-      if (err) return next(err);
-      console.log("Success!");
-      res.json(user);
-    });
+exports.delete = function (req, res, next) {
+	User.findByIdAndRemove(req.user.id, req.body, function (err, user) {
+		if (err) return next(err);
+		console.log("Success!");
+		res.json(user);
+	});
 };
 
 exports.list = function (req, res, next) {
-    // Use the 'User' instance's 'find' method to retrieve a new user document
-    User.find({}, function (err, users) {
-        if (err) {
-            return next(err);
-        } else {
-            res.json(users);
-        }
-    });
+	// Use the 'User' instance's 'find' method to retrieve a new user document
+	User.find({}, function (err, users) {
+		if (err) {
+			return next(err);
+		} else {
+			res.json(users);
+		}
+	});
 };
 
-exports.read = function(req, res) {
+exports.read = function (req, res) {
 	// Use the 'response' object to send a JSON response
 	res.json(req.user);
 };
@@ -60,55 +60,57 @@ exports.read = function(req, res) {
 exports.userByID = function (req, res, next, id) {
 	// Use the 'user' static 'findOne' method to retrieve a specific user
 	User.findOne({
-        _id: id
+		_id: id
 	}, (err, user) => {
 		if (err) {
 			// Call the next middleware with an error message
 			return next(err);
 		} else {
 			// Set the 'req.user' property
-            req.user = user;
-            console.log("user >>>", user);
+			req.user = user;
+			console.log("user >>>", user);
 			// Call the next middleware
 			next();
 		}
 	});
 };
 
-exports.authenticate = function(req, res, next) {
+exports.authenticate = function (req, res, next) {
 	// Get credentials from request
 	console.log(req.body)
 	const username = req.body.auth.username;
-	const password  = req.body.auth.password;
+	const password = req.body.auth.password;
 	console.log(password)
 	console.log(username)
 	//find the user with given userName using static method findOne
-	User.findOne({username: username}, (err, user) => {
-			if (err) {
-				return next(err);
-			} else {
-			console.log(">>>>>>"+user)
+	User.findOne({ username: username }, (err, user) => {
+		if (err) {
+			return next(err);
+		} else {
+			console.log(">>>>>>" + user)
 			//compare passwords	
-			if(bcrypt.compareSync(password, user.password)) {
+			if (bcrypt.compareSync(password, user.password)) {
 				// Create a new token with the user id in the payload
-  				// and which expires 300 seconds after issue
-				const token = jwt.sign({ id: user._id, username: user.username, usertype: user.usertype }, jwtKey, 
-					{algorithm: 'HS256', expiresIn: jwtExpirySeconds });
+				// and which expires 300 seconds after issue
+				const token = jwt.sign({ id: user._id, username: user.username, usertype: user.usertype }, jwtKey,
+					{ algorithm: 'HS256', expiresIn: jwtExpirySeconds });
 				console.log('token:', token)
 				// set the cookie as the token string, with a similar max age as the token
 				// here, the max age is in milliseconds
-				res.cookie('token', token, { maxAge: jwtExpirySeconds * 1000,httpOnly: true});
-				res.status(200).send({ screen: user.usertype, type:user.usertype });
-					console.log("success!")
+				res.cookie('token', token, { maxAge: jwtExpirySeconds * 1000, httpOnly: true });
+				res.status(200).send({ screen: user.usertype, type: user.usertype });
+				console.log("success!")
 				//call the next middleware
 				next()
 			} else {
-				res.json({status:"error", message: "Invalid username/password!!!",
-				data:null});
+				res.json({
+					status: "error", message: "Invalid username/password!!!",
+					data: null
+				});
 			}
-			
+
 		}
-		
+
 	});
 };
 //check if the user is signed in
@@ -124,24 +126,24 @@ exports.isSignedIn = (req, res) => {
 	// if the cookie is not set, return 'auth'
 	if (!token) {
 		console.log("token is null")
-	  return res.send({ screen: 'auth' }).end();
+		return res.send({ screen: 'auth' }).end();
 	}
 	var payload;
 	try {
-	  // Parse the JWT string and store the result in `payload`.
-	  // Note that we are passing the key in this method as well. This method will throw an error
-	  // if the token is invalid (if it has expired according to the expiry time we set on sign in),
-	  // or if the signature does not match
-	  payload = jwt.verify(token, jwtKey)
+		// Parse the JWT string and store the result in `payload`.
+		// Note that we are passing the key in this method as well. This method will throw an error
+		// if the token is invalid (if it has expired according to the expiry time we set on sign in),
+		// or if the signature does not match
+		payload = jwt.verify(token, jwtKey)
 	} catch (e) {
-	  if (e instanceof jwt.JsonWebTokenError) {
-		// the JWT is unauthorized, return a 401 error
-		return res.status(401).end()
-	  }
-	  // otherwise, return a bad request error
-	  return res.status(400).end()
+		if (e instanceof jwt.JsonWebTokenError) {
+			// the JWT is unauthorized, return a 401 error
+			return res.status(401).end()
+		}
+		// otherwise, return a bad request error
+		return res.status(400).end()
 	}
-  
+
 	// Finally, token is ok, return the userNumber given in the token
 	console.log("payload.usertype >> ", payload.usertype);
 	res.status(200).send({ screen: payload.usertype });
@@ -149,69 +151,119 @@ exports.isSignedIn = (req, res) => {
 
 exports.signout = (req, res) => {
 	res.clearCookie("token")
-	return res.status('200').json({message: "signed out"})
+	return res.status('200').json({ message: "signed out" })
 	// Redirect the user back to the main application page
 	//res.redirect('/');
 }
 
-exports.welcome = (req, res) => {
-	// We can obtain the session token from the requests cookies,
-	// which come with every request
-	const token = req.cookies.token
-  
-	// if the cookie is not set, return an unauthorized error
-	if (!token) {
-	  return res.status(401).end()
-	}
-   
-	var payload;
-	try {
-	  // Parse the JWT string and store the result in `payload`.
-	  // Note that we are passing the key in this method as well. This method will throw an error
-	  // if the token is invalid (if it has expired according to the expiry time we set on sign in),
-	  // or if the signature does not match
-	  payload = jwt.verify(token, jwtKey)
-	} catch (e) {
-	  if (e instanceof jwt.JsonWebTokenError) {
-		// if the error thrown is because the JWT is unauthorized, return a 401 error
-		return res.status(401).end()
-	  }
-	  // otherwise, return a bad request error
-	  return res.status(400).end()
-	}
-  
-
-	res.send(`${payload.username}!`)
- };
-
- //isAuthenticated() method to check whether a user is currently authenticated
+//isAuthenticated() method to check whether a user is currently authenticated
 exports.requiresLogin = function (req, res, next) {
-    // Obtain the session token from the requests cookies,
+	// Obtain the session token from the requests cookies,
 	// which come with every request
 	const token = req.cookies.token
 	console.log(token)
 	// if the cookie is not set, return an unauthorized error
 	if (!token) {
-	  return res.send({ screen: 'auth' }).end();
+		return res.send({ screen: 'auth' }).end();
 	}
 	var payload;
 	try {
-	  // Parse the JWT string and store the result in `payload`.
-	  // Note that we are passing the key in this method as well. This method will throw an error
-	  // if the token is invalid (if it has expired according to the expiry time we set on sign in),
-	  // or if the signature does not match
-	  payload = jwt.verify(token, jwtKey)
-	  console.log('in requiresLogin - payload:',payload)
-	  req.id = payload.id;
+		// Parse the JWT string and store the result in `payload`.
+		// Note that we are passing the key in this method as well. This method will throw an error
+		// if the token is invalid (if it has expired according to the expiry time we set on sign in),
+		// or if the signature does not match
+		payload = jwt.verify(token, jwtKey)
+		console.log('in requiresLogin - payload:', payload)
+		req.id = payload.id;
 	} catch (e) {
-	  if (e instanceof jwt.JsonWebTokenError) {
-		// if the error thrown is because the JWT is unauthorized, return a 401 error
-		return res.status(401).end()
-	  }
-	  // otherwise, return a bad request error
-	  return res.status(400).end()
+		if (e instanceof jwt.JsonWebTokenError) {
+			// if the error thrown is because the JWT is unauthorized, return a 401 error
+			return res.status(401).end()
+		}
+		// otherwise, return a bad request error
+		return res.status(400).end()
 	}
 	// user is authenticated
 	//call next function in line
-    next();
+	next();
 };
+
+function getErrorMessage(err) {
+    if (err.errors) {
+        for (let errName in err.errors) {
+            if (err.errors[errName].message) return err.errors[errName].
+                message;
+        }
+    } else {
+        return 'Unknown server error';
+    }
+};
+
+exports.searchPatient = (req, res) => {
+	console.log("searchPatient >>>>> ", req.body);
+
+	var searchType = req.body.type;
+	var searchStr = req.body.str;
+
+	if (searchType != '' && searchStr != '') {
+		console.log("NOT NULL");
+
+		// search type: id, firstname, lastname, email, birthOfDate
+		if (searchType == 'id') {
+			User.find({ username: searchStr, usertype: 'patient' }, function (err, users) {
+				if (err) {
+					return res.status(400).send({
+						message: getErrorMessage(err)
+					});
+				} else {
+					res.status(200).json(users);
+					console.log("user search result >>>>> ", users);
+				}
+			});
+		} else if (searchType == 'firstName') {
+			User.find({ firstname: searchStr, usertype: 'patient' }, function (err, users) {
+				if (err) {
+					return res.status(400).send({
+						message: getErrorMessage(err)
+					});
+				} else {
+					res.status(200).json(users);
+					console.log("user search result >>>>> ", users);
+				}
+			});
+		} else if (searchType == 'lastName') {
+			User.find({ lastname: searchStr, usertype: 'patient' }, function (err, users) {
+				if (err) {
+					return res.status(400).send({
+						message: getErrorMessage(err)
+					});
+				} else {
+					res.status(200).json(users);
+					console.log("user search result >>>>> ", users);
+				}
+			});
+		} else if (searchType == 'email') {
+			User.find({ email: searchStr, usertype: 'patient' }, function (err, users) {
+				if (err) {
+					return res.status(400).send({
+						message: getErrorMessage(err)
+					});
+				} else {
+					res.status(200).json(users);
+					console.log("user search result >>>>> ", users);
+				}
+			});
+		} else if (searchType == 'dateOfBirth') {
+			User.find({ dateOfbirth: searchStr, usertype: 'patient' }, function (err, users) {
+				if (err) {
+					return res.status(400).send({
+						message: getErrorMessage(err)
+					});
+				} else {
+					res.status(200).json(users);
+					console.log("user search result >>>>> ", users);
+				}
+			});
+		}
+	}
+}
--- react-ui/src/components/nurse/SearchPatient.js
+++ react-ui/src/components/nurse/SearchPatient.js
@@ -1,36 +1,151 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import axios from 'axios';
+import Spinner from 'react-bootstrap/Spinner';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+
+import Home from '../Home';
 
 function SearchPatient(props) {
     const [screen, setScreen] = useState('auth');
+    const [showLoading, setShowLoading] = useState(false);
+    const apiUrl = "http://localhost:3000/searchPatient";
+    const [search, setSearch] = useState({
+        type: '',
+        str: ''
+    });
+    const [result, setResult] = useState([]);
 
-  //check if the user already logged-in
-  const readCookie = async () => {
-    try {
-      console.log('--- in readCookie function ---');
+    //check if the user already logged-in
+    const readCookie = async () => {
+        try {
+            console.log('--- in readCookie function ---');
 
-      //
-      const res = await axios.get('/read_cookie');
-      // 
-      if (res.data.screen !== undefined) {
-        setScreen(res.data.screen);
-        console.log(res.data.screen)
-      }
-    } catch (e) {
-      setScreen('auth');
-      console.log(e);
+            //
+            const res = await axios.get('/read_cookie');
+            // 
+            if (res.data.screen !== undefined) {
+                setScreen(res.data.screen);
+                console.log(res.data.screen)
+            }
+        } catch (e) {
+            setScreen('auth');
+            console.log(e);
+        }
+    };
+    //runs the first time the view is rendered
+    //to check if user is signed in
+    useEffect(() => {
+        readCookie();
+    }, []); //only the first render
+
+
+    const searchPatient = (e) => {
+        setShowLoading(true);
+        e.preventDefault();
+
+        if (search.type == '') {
+            search.type = 'id';
+        }
+        const data = {
+            type: search.type,
+            str: search.str
+        };
+        console.log("data >>>>>>>>>> ", data);
+
+        axios.post(apiUrl, data).then((result) => {
+            setShowLoading(false);
+
+            console.log('result.data:', result.data)
+            //check if the user has logged in
+            if (result.data.screen !== 'auth') {
+                console.log('data in if:', result.data)
+                setResult(result.data);
+                setShowLoading(false);
+            }
+
+        }).catch((error) => setShowLoading(false));
+    };
+
+    const onChange = (e) => {
+        e.persist();
+        setSearch({ ...search, [e.target.name]: e.target.value });
     }
-  };
-  //runs the first time the view is rendered
-  //to check if user is signed in
-  useEffect(() => {
-    readCookie();
-  }, []); //only the first render
 
     return (
         <div>
-            <h2>Search Patient: {screen}</h2>
+            {showLoading &&
+                <Spinner animation="border" role="status">
+                    <span className="sr-only">Loading...</span>
+                </Spinner>
+            }
+
+            {screen === 'auth'
+                ?
+                <Home screen={screen} setScreen={setScreen} />
+                :
+                <div>
+                    <h2>Search Patient: {screen}</h2>
+
+                    <form onSubmit={searchPatient}>
+
+                        <select id="type" name="type" onChange={onChange}>
+                            <option value="id">ID</option>
+                            <option value="firstName">First Name</option>
+                            <option value="lastName">Last Name</option>
+                            <option value="email">Email Address</option>
+                            <option value="dateOfBirth">Date Of Birth</option>
+                        </select>
+
+                        <input type="text" id="str" name="str" onChange={onChange} />
+
+                        <button type="submit">Search</button>
+                    </form>
+                </div>
+            }
+
+            {result.length !== 0
+                ?
+                <div>
+                    <h2 className="Title">Patient Search Result</h2>
+                    <br />
+                    <br />
+                    <Table responsive>
+                        <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">First Name</th>
+                                <th scope="col">Last Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Phone Number</th>
+                                <th scope="col">Date Of Birth</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {result.map((item, idx) => (
+                                <tr>
+                                    <td>{item.username}</td>
+                                    <td>{item.firstname}</td>
+                                    <td>{item.lastname}</td>
+                                    <td>{item.email}</td>
+                                    <td>{item.phoneNumber}</td>
+                                    <td>{item.dateOfbirth}</td>
+                                    <td>
+                                        <Button className="ButtonSpace">Detail</Button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                    <br />
+                    <h5 className="Title">Total: {result.length}</h5>
+                </div>
+                :
+                <div></div>
+            }
+
         </div>
     );
 
