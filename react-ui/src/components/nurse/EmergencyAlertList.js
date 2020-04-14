bdca2e75baf587461d39831fc8644cd6937049e9
HarleyQuinna <sylviachen627@gmail.com>
2020-04-14 04:47:37 -0400

    updated emergency list update

--- react-ui/src/components/nurse/EmergencyAlertList.js
+++ react-ui/src/components/nurse/EmergencyAlertList.js
@@ -9,7 +9,7 @@ import Table from 'react-bootstrap/Table';
 function EmergencyAlertList(props) {
   const [data, setData] = useState([]);
   const [showLoading, setShowLoading] = useState(true);
-  const apiUrl = "http://localhost:3000/emergencies";
+  const apiUrl = "http://localhost:3000/emergencies/";
 
 
   useEffect(() => {
@@ -23,13 +23,45 @@ function EmergencyAlertList(props) {
     fetchData();
   }, []);
 
+  const markAsResolved = (emergencyId, emergency) => {
+    const updateUrl = apiUrl + emergencyId;
+    const data = { emergencyCode: emergency.emergencyCode, 
+        emergencySubject: emergency.emergencySubject, 
+        description: emergency.description,
+        contactName: emergency.contactName,
+        contactNumber: emergency.contactNumber,
+        creationTime: emergency.creationTime,
+        status: "Resolved",
+     };
+     axios.put(updateUrl, data)
+     .then((result) => {
+        window.location.reload(false)
+     })
+  }
+
+  const closeAlert = (emergencyId, emergency) => {
+    const updateUrl = apiUrl + emergencyId;
+    const data = { emergencyCode: emergency.emergencyCode, 
+        emergencySubject: emergency.emergencySubject, 
+        description: emergency.description,
+        contactName: emergency.contactName,
+        contactNumber: emergency.contactNumber,
+        creationTime: emergency.creationTime,
+        status: "Closed",
+     };
+     axios.put(updateUrl, data)
+     .then((result) => {
+        window.location.reload(false)
+     })
+  }
+
   return (
     <div>
       {showLoading && <Spinner animation="border" role="status">
         {/* <span className="sr-only">Loading...</span> */}
       </Spinner> }
 
-      <h2 style={{textAlign: "center"}}>Emergency Alert List</h2>
+      <h2 style={{textAlign: "center"}}>Received Emergency Alerts</h2>
       <Table striped bordered hover style= {{marginTop: 20, width: 1200, marginLeft: 150}}>
             <thead>
               <tr>
@@ -39,8 +71,9 @@ function EmergencyAlertList(props) {
                 <th>Contact Name</th>
                 <th>Contact Number</th>
                 <th>Creation Time</th>
-                <th>Patient</th>
+                <th>Patient Email</th>
                 <th>Status</th>
+                <th>Action</th>
               </tr>
             </thead>
             
@@ -54,7 +87,37 @@ function EmergencyAlertList(props) {
             <td>{item.contactNumber}</td>
             <td>{item.creationTime}</td>
             <td>{item.patientId}</td>
-            <td style={{color: 'green'}}>{item.status}</td>
+            <td>
+            {item.status === 'Resolved'
+            ?
+            <p style={{color: 'green'}}>{item.status}</p>
+            : <div>
+                {item.status === 'Closed' ?
+                <p style={{color: 'red'}}>{item.status}</p>
+                : <div>
+                     {item.status === 'Active' ?
+                <p style={{color: 'blue'}}>{item.status}</p>
+                :             <p style={{color: 'blue'}}>{item.status}</p>}
+                </div>
+                }
+            </div>
+
+            }
+
+           
+            </td>
+            
+             
+            {item.status === 'Closed' || item.status ==='Resolved'
+            ? 
+            <td>This emergency alert no longer needs any operations </td>
+            :
+            
+            <td>
+              <Button style={{width: 180, height: 35}} variant="success" onClick={ () => markAsResolved(item._id, item)}>Mark as Resolved</Button>
+              <Button style={{width: 180, height: 35, marginTop: 15}} variant="danger" onClick={() => closeAlert(item._id, item)}>Close</Button>
+              </td>
+}<div></div>
               </tr>
           
         ))}
