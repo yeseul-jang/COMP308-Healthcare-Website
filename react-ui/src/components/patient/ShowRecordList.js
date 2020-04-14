bf9a17a6b7fc6836300184ab097445cad232bc20
HarleyQuinna <sylviachen627@gmail.com>
2020-04-14 06:56:52 -0400

    updated daily record by patients

---
+++ react-ui/src/components/patient/ShowRecordList.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Spinner from 'react-bootstrap/Spinner';
+import { withRouter } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
+
+function DailyRecordList(props) {
+  const [data, setData] = useState([]);
+  const [showLoading, setShowLoading] = useState(true);
+  const apiUrl = "http://localhost:3000/dailyrecordslist/" + props.match.params.email;
+
+  useEffect(() => {
+    setShowLoading(false);
+    const fetchData = async () => {
+      const result = await axios(apiUrl);
+      setData(result.data);
+      setShowLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+
+  return (
+    <div>
+      {showLoading && <Spinner animation="border" role="status">
+        {/* <span className="sr-only">Loading...</span> */}
+      </Spinner> }
+
+      <h2 style={{textAlign: "center", color: "green"}}>Your Daily Health Record</h2>
+      <Table striped bordered hover style= {{marginTop: 20, width: 1200, marginLeft: 150}}>
+            <thead>
+              <tr>
+                <th>Creation Time</th>
+                <th>Nurse Practitioner</th>
+                <th>Pulse Rate</th>
+                <th>Blood Pressure</th>
+                <th>Weight</th>
+                <th>Body Temperature</th>
+                <th>Respiratory Rate</th>
+              </tr>
+            </thead>
+            
+            <tbody>
+            {data.map((item, idx) => (
+              <tr key={idx}>
+            <td >{item.creationTime}</td>
+            <td>{item.nurseUsername}</td>
+            <td>{item.pulseRate}</td>
+            <td>{item.bloodPressure}</td>
+            <td>{item.weight}</td>
+            <td>{item.temperature}</td>
+            <td>{item.respiratoryRate}</td>
+
+              </tr>
+          
+        ))}
+              </tbody>
+            </Table>
+    </div>
+  );
+}
+
+export default withRouter(DailyRecordList);
