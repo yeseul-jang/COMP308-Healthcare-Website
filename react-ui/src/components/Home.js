9819d9f540956cbf41353d8a22f5e36a226462b8
KyungjinJeong <57504664+KyungjinJeong@users.noreply.github.com>
2020-04-14 00:33:37 -0400

    merge

--- react-ui/src/components/Home.js
+++ react-ui/src/components/Home.js
@@ -1,13 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import axios from 'axios';
-<<<<<<< HEAD
 import homeImg from './home.png';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-=======
-import View from './View'
->>>>>>> b1309673946de7d178893507f01776aab11317d6
 
 function Home(props) {
     const [screen, setScreen] = useState('auth');
@@ -63,7 +59,6 @@ function Home(props) {
 
     return (
         <div>
-<<<<<<< HEAD
             <div className="home">
                 {/* <h1>HOME: {screen}</h1> */}
 
@@ -133,19 +128,6 @@ function Home(props) {
                 <div></div>
             }
 
-=======
-          {screen === 'patient'
-          ?
-          <View/>
-          :
-          <div>
-            <h1>HOME: {screen}</h1>
-            <h2> Hero Healthcare Website</h2>
-            <p> created by Hero Group</p>
-          </div>
-        }
-            
->>>>>>> b1309673946de7d178893507f01776aab11317d6
         </div>
     );
 
